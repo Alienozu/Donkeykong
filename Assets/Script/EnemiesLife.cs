2c469d10c773c57a0ae85079676c841cd9791444
Mimosa0314 <myfow14@gmail.com>
2025-06-25 23:07:22 +0900

    Enemyのベースクラスを追加、及びスポーン時の位置調整を追加（未完）

--- Assets/Script/EnemiesLife.cs
+++ Assets/Script/EnemiesLife.cs
@@ -1,20 +1,72 @@
 using System.Collections;
 using UnityEngine;
 
-public class EnemiesLife : MonoBehaviour
+
+public class EnemyBase : MonoBehaviour
 {
-    Rigidbody2D rb;
+    protected Rigidbody2D enemyRigidbody;
+    protected BoxCollider2D enemyCollider;
+
+    protected bool isVisible;
+    protected Vector2 rayerEdge;
+
+    protected bool facedLeft = true;
+
+    private void Awake()
+    {
+        enemyRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        enemyCollider = gameObject.GetComponent<BoxCollider2D>();
+        isVisible = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isVisible)
+        {
+            Physics2D.SyncTransforms(); // コライダーの更新を強制
+            LayerMask groundLayer = LayerMask.GetMask("Ground", "Slope");
+
+            //スポーン位置の調整
+            rayerEdge = enemyCollider.bounds.min;
+            RaycastHit2D rayDown = Physics2D.Raycast(rayerEdge, Vector2.down, 100, groundLayer);
+            RaycastHit2D rayUp = Physics2D.Raycast(rayerEdge, Vector2.up, 100, groundLayer);
+
+            Transform sponeTransform = gameObject.transform;
+            Vector2 sponePos = sponeTransform.position;
+
+            if (rayDown.collider != null)
+            {
+                sponePos.y = rayDown.point.y + enemyCollider.bounds.extents.y+4f;
+                sponeTransform.position = sponePos;
+            }
+            if (rayUp.collider != null)
+            {
+                sponePos.y = rayUp.point.y;
+                sponeTransform.position = sponePos;
+
+            }
+            if (rayDown.collider != null && rayUp.collider != null)
+            {
+                Debug.LogError("複数のスポーンポイントが見つかりました。手動で設置してください");
+            }
+        }
+
+    }
+}
+public class EnemiesLife : EnemyBase
+{
     SpriteRenderer sr;
     private bool die;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private EdgeCollider2D stampedCollider;
+
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        enemyRigidbody = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        stampedCollider = gameObject.GetComponent<EdgeCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -35,11 +87,10 @@ public class EnemiesLife : MonoBehaviour
             GameObject pla = collision.gameObject;
             Rigidbody2D collRigidbody = pla.GetComponent<Rigidbody2D>();
 
-            if (collRigidbody.velocity.y <= 0)
+            if (collRigidbody.velocity.y < 0)
             {
                 DieEnemy();
             }
-
         }
     }
 
@@ -48,7 +99,7 @@ public class EnemiesLife : MonoBehaviour
     private void DieEnemy()
     {
         gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        rb.bodyType = RigidbodyType2D.Kinematic;
+        enemyRigidbody.bodyType = RigidbodyType2D.Kinematic;
 
         sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 70 / 255f);
         die = true;
@@ -61,6 +112,4 @@ public class EnemiesLife : MonoBehaviour
         yield return new WaitForSeconds(deleyTime);
         Destroy(gameObject);
     }
-
-
 }
