f792e0e18b2b9bbe8d84b01449372469ddc1dcc8
Mimosa0314 <myfow14@gmail.com>
2025-06-25 23:27:21 +0900

    基本的な動作を追加

--- Assets/Script/Rabbit.cs
+++ Assets/Script/Rabbit.cs
@@ -1,51 +1,105 @@
 using UnityEngine;
 
-public class Rabbit : MonoBehaviour
+public class Rabbit : EnemyBase
 {
-    private bool isVisible = false;
+    private Transform tr;
 
-    private bool facedLeft = true;   // 方向変換
 
-    private Transform trRabbit;
-    private Rigidbody2D rbRabbit;
-    private BoxCollider2D colliRabbit;
-
-    private Vector2 rayerEdge;
-
+    private bool isGround;
+    private int jumpCount;
+    private float coolTime;
 
     //プレイヤ
     private GameObject Player;
     private Transform playerPosition;
     private Vector2 playerPos;
-    private float playerPosX;
+   
 
+
+
+    /*ウサギの設定
+     * 方向転換や地面の認識は行わない。
+     * プレイヤーがいる方向に対して向かうようにする予定
+     */
+
+    private void Awake()
+    {
+        tr = gameObject.GetComponent<Transform>();
+        enemyRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        enemyCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        isVisible = false;
+    }
     void Start()
     {
-        trRabbit = gameObject.GetComponent<Transform>();
-        rbRabbit = gameObject.GetComponent<Rigidbody2D>();
-        colliRabbit = gameObject.GetComponent<BoxCollider2D>();
-
-
-        Player = GameObject.Find("Playwer");
+        Player = GameObject.Find("Player");
         playerPosition = Player.GetComponent<Transform>();
+        playerPos = playerPosition.position;
+
+        isGround = true;
+        jumpCount = 0;
     }
 
 
     void Update()
-    {
+    {                 
         if (isVisible)
         {
+            Vector2 sca = tr.localScale;
 
+            int direction = facedLeft ? -1 : 1;
+            //基本的な挙動
+            if (isGround)
+            {
+
+                coolTime -= Time.deltaTime;
+
+                if (coolTime <= 0 && jumpCount == 1)
+                {
+                    Vector2 jump = new Vector2(direction * 5f, 5f);
+                    enemyRigidbody.AddForce(jump, ForceMode2D.Impulse);
+
+                    jumpCount--;
+                }
+            }
+            
+
+            //プレイヤーに合わせ方向転換
+            if (playerPosition.position.x > tr.position.x)
+            {
+                sca.x = Mathf.Abs(sca.x);
+                tr.localScale = sca;
+                facedLeft = false;
+            }
+            else if (playerPosition.position.x < tr.position.x)
+            {
+                sca.x = -Mathf.Abs(sca.x);
+                tr.localScale = sca;
+                facedLeft = true;
+            }
         }
     }
 
     private void OnBecameVisible()
     {
-        if (isVisible)
-        {
-
-        }
         isVisible = true;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+            jumpCount++;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
+            coolTime = 1.5f;
+        }
+
+    }
 }
